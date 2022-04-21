d8a7f683a3cee791fe18278afae47fe679ad9bdf
Abel Velasquez <velasquez.abel@gmail.com>
2022-04-21 19:58:25 +0000

    agregando template base en gradle para backend en java

---
+++ java-backend/src/test/java/cl/abelvelasquez/fscoo/FscooApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.abelvelasquez.fscoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FscooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
